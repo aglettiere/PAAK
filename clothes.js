104b8e376d622d124e1bc219ff7669f55fa1b4a6
aglettiere <allisonglettiere@gmail.com>
2015-08-11 15:32:34 -0500

    Start for the clothing page

---
+++ clothes.js
@@ -0,0 +1,16 @@
+var list=[["Preppy","Inverted Triangle","https://www.countryclubprep.com/collections/dresses-and-skirts/products/the-classic-sleeveless-dress-in-peacoat-navy-and-white-by-sail-to-sable?variant=3470269953","Sail to Sable","Classic Sleeveless Dress In Peacoat Navy",188, "https://cdn.shopify.com/s/files/1/0731/0945/products/Striped_Tunic_Dress_in_Navy_and_White_by_Sail_to_Sable_v2_1024x1024.jpg?v=1435815898"],["Bohemian","Pear","http://www.forever21.com/Product/Product.aspx?BR=f21&Category=dress&ProductID=2000095044&VariantID=","Forever 21","Lace-Up Abstract Print Dress",29.90,"http://www.forever21.com/images/default_750/00095044-02.jpg"]];
+var clothes=[];
+for (var i=O; i<list.length; i++){
+	if (list[i][0] = style){
+		if (list[i][1]=shape){
+			if (list[i][5]<=price){
+				clothes.push(i);
+			}
+		}
+	}
+}
+
+
+
+
+FOR SELECTIONS SET STYLE TO THE ONE SELECTED
